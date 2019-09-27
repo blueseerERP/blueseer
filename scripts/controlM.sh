3a4cff6677eac15bf8ae6267e19ac8b803975591
Terry Vaughn <vaughnte222@gmail.com>
2019-09-27 11:04:54 -0400

    change

--- scripts/controlM.sh
+++
@@ -1,1 +0,0 @@
-find . -type f | xargs -Ix sed -i.bak -r 's/\r//g' x
--- scripts/etran.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-rem -if, -of, -id, -od, -m, -x
-java -cp ".;c:\bs\blueseer\scripts\test;c:\bs\blueseer\dist\*" com.blueseer.edi.EDIbs %*
