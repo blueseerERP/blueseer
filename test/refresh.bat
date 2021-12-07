33472d7d34cef9296729855bb9d6ed34f7c97bf5
vaughnte <vaughnte222@gmail.com>
2021-12-07 09:36:46 -0500

    dev script fix

--- test/refresh.bat
+++ test/refresh.bat
@@ -1,31 +1,29 @@
 @echo off
 
-@echo "Installing BlueSeer to test\scratch.... "
+@echo "Refresh a BlueSeer test instance.... "
 @echo ""
 @echo ""
 
-cd %~dp0\scratch
+cd %~dp0
 
 set "DB=bsdb"
 
-@echo "creating bsconfig file...."
-@echo DBTYPE=sqlite>>bsconfig
-@echo DB=data\bsdb.db>>bsconfig
-@echo USER=bs_user>>bsconfig
-@echo PASS=bspasswd>>bsconfig
-@echo IP=localhost>>bsconfig
-@echo PORT=3306>>bsconfig
-@echo DRIVER=org.sqlite.JDBC>>bsconfig
+@echo "creating bs.cfg file...."
+@echo DBTYPE=sqlite>>bs.cfg
+@echo DB=data\bsdb.db>>bs.cfg
+@echo USER=bs_user>>bs.cfg
+@echo PASS=bspasswd>>bs.cfg
+@echo IP=localhost>>bs.cfg
+@echo PORT=3306>>bs.cfg
+@echo DRIVER=org.sqlite.JDBC>>bs.cfg
+@echo LANGUAGE=en>>bs.cfg
+@echo COUNTRY=US>>bs.cfg
 
-if exist ..\.patch copy ..\.patch %~dp0\scratch
-
-cd data
-
-if exist bsdb.db del bsdb.db
-
-@echo "creating database schema...."
-sqlite3.exe bsdb.db <blueseer.sqlite
-sqlite3.exe bsdb.db <sq.txt
+if exist data rmdir data
+mkdir data
+if exist ..\.patch copy ..\.patch %~dp0\
+if exist ..\sf\data\en copy ..\sf\data\en\* %~dp0\data\
+if exist ..\sf\data\bsdbdev.db copy ..\sf\data\bsdbdev.db %~dp0\data\bsdb.db
 
 
 @echo ""
