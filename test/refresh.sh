b48e623eae253c23f99e9f2e0926548d68cc4efd
vaughnte <vaughnte222@gmail.com>
2022-07-03 10:30:15 -0400

    EOL mod

--- test/refresh.sh
+++ test/refresh.sh
@@ -21,4 +21,3 @@ cp ../scripts/bs.cfg bs.cfg
 
 echo "NOTE!!  you will need to adjust the property name 'JDK17dir' in the build.xml file for your JDK17 path"
 echo "assign the path to the parameter as (example) /some/path/to/jdk-17  "
-
