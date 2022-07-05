f07c2adee6db61794093420d2528850fa81bc15b
vaughnte <vaughnte222@gmail.com>
2022-07-05 09:36:01 -0400

    proc cleanup

--- scripts/bszip.ps1
+++ scripts/bszip.ps1
@@ -23,8 +23,22 @@ foreach ($line in $filecontent) {
 $jaronly = "blueseer.jaronly.zip"
 rm $wip\$jaronly
 compress-archive -update -path ..\dist\blueseer.jar -destinationpath $wip\$jaronly
+compress-archive -update -path ..\dist\bsmf.jar -destinationpath $wip\$jaronly
 compress-archive -update -path ..\.patch -destinationpath $wip\$jaronly
 compress-archive -update -path instructions.txt -destinationpath $wip\$jaronly
+compress-archive -update -path ..\dist\icepdf-core-6.2.2.jar -destination $wip\$jaronly
+compress-archive -update -path ..\dist\icepdf-viewer-6.2.2.jar -destination $wip\$jaronly
+compress-archive -update -path ..\dist\commons-dbcp2-2.7.0.jar -destination $wip\$jaronly
+compress-archive -update -path ..\dist\eddsa-0.3.0.jar -destination $wip\$jaronly
+compress-archive -update -path ..\dist\sshj-0.32.0.jar -destination $wip\$jaronly
+compress-archive -update -path ..\dist\sshd-core-2.8.0.jar -destination $wip\$jaronly
+compress-archive -update -path ..\dist\bcpg-jdk18on-171.jar -destination $wip\$jaronly
+compress-archive -update -path ..\dist\bcmail-jdk18on-171.jar -destination $wip\$jaronly
+compress-archive -update -path ..\dist\bcprov-jdk18on-171.jar -destination $wip\$jaronly
+compress-archive -update -path ..\dist\bcutil-jdk18on-171.jar -destination $wip\$jaronly
+compress-archive -update -path ..\dist\bctls-jdk18on-171.jar -destination $wip\$jaronly
+compress-archive -update -path ..\dist\bcpkix-jdk18on-171.jar -destination $wip\$jaronly
+
 
 
 rm blueseer.patch.*.zip
@@ -41,10 +55,20 @@ cp ..\sf\patches\patch_instructions.pdf $patchdir\
 cp ..\sf\jasper $patchdir\ -recurse
 cp ..\sf\zebra $patchdir\ -recurse
 # cp ..\dist $patchdir\ -recurse
-cp ..\dist\blueseer.jar $patchdir\dist\
+cp ..\dist\bsmf.jar $patchdir\dist\bsmf.jar
 cp ..\dist\blueseer.jar $patchdir\dist\blueseer.jar
 cp ..\dist\icepdf-core-6.2.2.jar $patchdir\dist\icepdf-core-6.2.2.jar
 cp ..\dist\icepdf-viewer-6.2.2.jar $patchdir\dist\icepdf-viewer-6.2.2.jar
+cp ..\dist\commons-dbcp2-2.7.0.jar $patchdir\dist\commons-dbcp2-2.7.0.jar
+cp ..\dist\eddsa-0.3.0.jar $patchdir\dist\eddsa-0.3.0.jar
+cp ..\dist\sshj-0.32.0.jar $patchdir\dist\sshj-0.32.0.jar
+cp ..\dist\sshd-core-2.8.0.jar $patchdir\dist\sshd-core-2.8.0.jar
+cp ..\dist\bcpg-jdk18on-171.jar $patchdir\dist\bcpg-jdk18on-171.jar
+cp ..\dist\bcmail-jdk18on-171.jar $patchdir\dist\bcmail-jdk18on-171.jar
+cp ..\dist\bcprov-jdk18on-171.jar $patchdir\dist\bcprov-jdk18on-171.jar
+cp ..\dist\bcutil-jdk18on-171.jar $patchdir\dist\bcutil-jdk18on-171.jar
+cp ..\dist\bctls-jdk18on-171.jar $patchdir\dist\bctls-jdk18on-171.jar
+cp ..\dist\bcpkix-jdk18on-171.jar $patchdir\dist\bcpkix-jdk18on-171.jar
 compress-archive -force -path $patchdir -destinationpath $wip\$patchzip
 rmdir $patchdir -force -recurse
 
