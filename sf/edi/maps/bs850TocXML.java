f5a30b1f22a45442d341a247284f1bb191fd54a9
vaughnte <vaughnte222@gmail.com>
2022-12-10 09:26:13 -0500

    added new map and struc for cXML

---
+++ sf/edi/maps/bs850TocXML.java
@@ -0,0 +1,33 @@
+// Example methods
+
+GlobalDebug = true;
+
+mapSegment("cXML","timestamp","2012-12-12T12:12:12");
+mapSegment("cXML","version","1.25");
+mapSegment("cXML","MeansDescriptionCode","1.25");
+mapSegment("cXML","payloadID","999");
+commitSegment("cXML");
+
+ mapSegment("cXML:Header:From:Credential","Identity","XXX");
+mapSegment("cXML:Header:From:Credential","domain","Company");
+mapSegment("cXML:Header:From:Credential","foo","hoo");
+ commitSegment("cXML:Header:From:Credential");
+
+
+mapSegment("cXML:Request:OrderRequest:OrderRequestHeader:ShipTo:Address","Name",getInput("N1","1:ST",2));
+mapSegment("cXML:Request:OrderRequest:OrderRequestHeader:ShipTo:Address","addressID",getInput("N1","1:ST",4));
+mapSegment("cXML:Request:OrderRequest:OrderRequestHeader:ShipTo:Address","isoCountryCode","US");
+mapSegment("cXML:Request:OrderRequest:OrderRequestHeader:ShipTo:Address","ReturnData","en");
+commitSegment("cXML:Request:OrderRequest:OrderRequestHeader:ShipTo:Address");
+
+mapSegment("cXML:Request:OrderRequest:OrderRequestHeader:ShipTo:Address:PostalAddress","DeliverTo",getInput("N1","1:ST",2));
+commitSegment("cXML:Request:OrderRequest:OrderRequestHeader:ShipTo:Address:PostalAddress");
+
+mapSegment("cXML:Request:OrderRequest:OrderRequestHeader:BillTo:Address","Name",getInput("N1","1:BT",2));
+mapSegment("cXML:Request:OrderRequest:OrderRequestHeader:BillTo:Address","addressID",getInput("N1","1:BT",4));
+mapSegment("cXML:Request:OrderRequest:OrderRequestHeader:BillTo:Address","isoCountryCode","US");
+mapSegment("cXML:Request:OrderRequest:OrderRequestHeader:BillTo:Address","ReturnData","en");
+mapSegment("cXML:Request:OrderRequest:OrderRequestHeader:BillTo:Address","addressname","BOOHOO");
+commitSegment("cXML:Request:OrderRequest:OrderRequestHeader:BillTo:Address");
+
+
