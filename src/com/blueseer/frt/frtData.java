6eee50e477bd52d70f50a0df5c02a52b34556802
vaughnte <vaughnte222@gmail.com>
2023-02-22 10:21:46 -0500

    update to carmaint crud methods

--- src/com/blueseer/frt/frtData.java
+++ src/com/blueseer/frt/frtData.java
@@ -49,8 +49,11 @@ public class frtData {
     public static String[] addCarrierMstr(car_mstr x) {
         String[] m = new String[2];
         String sqlSelect = "SELECT * FROM  car_mstr where car_id = ?";
-        String sqlInsert = "insert into car_mstr (car_id, car_desc, car_apply ) "
-                        + " values (?,?,?); "; 
+        String sqlInsert = "insert into car_mstr (car_id, car_desc, car_apply, car_scac, car_name, car_line1, car_line2, car_city, " +
+"        car_state, car_zip, car_country, car_phone, car_email, " +
+"        car_type, car_acct, car_usdot, car_mc, car_ein, " +
+"        car_minmiles, car_maxmiles, car_maxdh, car_milerate ) "
+                        + " values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?); "; 
         try (Connection con = (ds == null ? DriverManager.getConnection(url + db, user, pass) : ds.getConnection()); 
              PreparedStatement ps = con.prepareStatement(sqlSelect);) {
              ps.setString(1, x.car_id);
@@ -60,6 +63,26 @@ public class frtData {
             psi.setString(1, x.car_id);
             psi.setString(2, x.car_desc);
             psi.setString(3, x.car_apply);
+            psi.setString(4, x.car_scac);
+            psi.setString(5, x.car_name);
+            psi.setString(6, x.car_line1);
+            psi.setString(7, x.car_line2);
+            psi.setString(8, x.car_city);
+            psi.setString(9, x.car_state);
+            psi.setString(10, x.car_zip);
+            psi.setString(11, x.car_country);
+            psi.setString(12, x.car_phone);
+            psi.setString(13, x.car_email);
+            psi.setString(14, x.car_type);
+            psi.setString(15, x.car_acct);
+            psi.setString(16, x.car_usdot);
+            psi.setString(17, x.car_mc);
+            psi.setString(18, x.car_ein);
+            psi.setString(19, x.car_minmiles);
+            psi.setString(20, x.car_maxmiles);
+            psi.setString(21, x.car_maxdh);
+            psi.setString(22, x.car_milerate);
+        
             int rows = psi.executeUpdate();
             m = new String[] {BlueSeerUtils.SuccessBit, BlueSeerUtils.addRecordSuccess};
             } else {
@@ -78,13 +101,35 @@ public class frtData {
 
     public static String[] updateCarrierMstr(car_mstr x) {
         String[] m = new String[2];
-        String sql = "update car_mstr set car_desc = ?, car_apply = ? " +   
-                          " where car_id = ? ; ";
+        String sql = "update car_mstr set car_desc = ?, car_apply = ?, car_scac = ?, car_name = ?, " +   
+                     " car_line1 = ?, car_line2 = ?, car_city = ?, car_state = ?, car_zip = ? car_country = ?, " +
+                     " car_phone = ?, car_email = ?, car_type = ?, car_acct = ?, car_usdot = ?, car_mc = ?, " +
+                     " car_ein = ?, car_minmiles = ?, car_maxmiles = ?, car_maxdh = ?, car_milerate = ? " +
+                     " where car_id = ? ; ";
         try (Connection con = (ds == null ? DriverManager.getConnection(url + db, user, pass) : ds.getConnection()); 
 	PreparedStatement ps = con.prepareStatement(sql)) {
         ps.setString(1, x.car_desc);
         ps.setString(2, x.car_apply);
-        ps.setString(3, x.car_id);
+        ps.setString(3, x.car_scac);
+        ps.setString(4, x.car_name);
+        ps.setString(5, x.car_line1);
+        ps.setString(6, x.car_line2);
+        ps.setString(7, x.car_city);
+        ps.setString(8, x.car_state);
+        ps.setString(9, x.car_zip);
+        ps.setString(10, x.car_country);
+        ps.setString(11, x.car_phone);
+        ps.setString(12, x.car_email);
+        ps.setString(13, x.car_type);
+        ps.setString(14, x.car_acct);
+        ps.setString(15, x.car_usdot);
+        ps.setString(16, x.car_mc);
+        ps.setString(17, x.car_ein);
+        ps.setString(18, x.car_minmiles);
+        ps.setString(19, x.car_maxmiles);
+        ps.setString(20, x.car_maxdh);
+        ps.setString(21, x.car_milerate);
+        ps.setString(22, x.car_id);
         int rows = ps.executeUpdate();
         m = new String[] {BlueSeerUtils.SuccessBit, BlueSeerUtils.updateRecordSuccess};
         } catch (SQLException s) {
@@ -110,7 +155,26 @@ public class frtData {
                         m = new String[]{BlueSeerUtils.SuccessBit, BlueSeerUtils.getRecordSuccess};
                         r = new car_mstr(m, res.getString("car_id"), 
                             res.getString("car_desc"),
-                            res.getString("car_apply")
+                            res.getString("car_apply"),
+                            res.getString("car_scac"),
+                            res.getString("car_name"),
+                            res.getString("car_line1"),
+                            res.getString("car_line2"),
+                            res.getString("car_city"),
+                            res.getString("car_state"),
+                            res.getString("car_zip"),
+                            res.getString("car_country"), 
+                            res.getString("car_phone"),
+                            res.getString("car_email"),
+                            res.getString("car_type"),
+                            res.getString("car_acct"),
+                            res.getString("car_usdot"),
+                            res.getString("car_mc"),
+                            res.getString("car_ein"),
+                            res.getString("car_minmiles"),
+                            res.getString("car_maxmiles"),
+                            res.getString("car_maxdh"),
+                            res.getString("car_milerate")
                         );
                     }
                 }
@@ -138,9 +202,15 @@ public class frtData {
         return m;
     }
     
-    public record car_mstr (String[] m, String car_id, String car_desc, String car_apply) {
+    public record car_mstr (String[] m, String car_id, String car_desc, String car_apply,
+        String car_scac, String car_name, String car_line1, String car_line2, String car_city,
+        String car_state, String car_zip, String car_country, String car_phone, String car_email,
+        String car_type, String car_acct, String car_usdot, String car_mc, String car_ein,
+        String car_minmiles, String car_maxmiles, String car_maxdh, String car_milerate) {
         public car_mstr(String[] m) {
-            this(m,"","","");
+            this(m,"","","","","","","","","","",
+                   "","","","","","","","","","",
+                   "","");
         }
     } 
     
