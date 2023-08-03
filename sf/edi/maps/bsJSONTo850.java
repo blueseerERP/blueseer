4380e7536397fa53f7530df6fbb20a9214ff2488
vaughnte <vaughnte222@gmail.com>
2023-08-03 11:22:41 -0400

    getJson mods to demo map

--- sf/edi/maps/bsJSONTo850.java
+++ sf/edi/maps/bsJSONTo850.java
@@ -12,55 +12,54 @@ mapSegment("REF","e01","XX");
 mapSegment("REF","e02",getInput("order","reference"));
 commitSegment("REF");
 
-int addrcount = getGroupCount("addresses:address");
+int addrcount = getLoopCount("order:addresses:address",2);
 for (int i = 1; i <= addrcount; i++) {
-
-if (getInput(i,"addresses:address","type").equals("billto")) {
+if (getInput(i,"order:addresses:address","type").equals("billto")) {
 mapSegment("N1","e01","BT");
-mapSegment("N1","e02",getInput(i,"addresses:address","name"));
+mapSegment("N1","e02",getInput(i,"order:addresses:address","name"));
 mapSegment("N1","e03","92");
-mapSegment("N1","e04",getInput(i,"addresses:address","addrid"));
+mapSegment("N1","e04",getInput(i,"order:addresses:address","addrid"));
 commitSegment("N1");
-mapSegment("N3:N1","e01",getInput(i,"addresses:address","line1"));
+mapSegment("N3:N1","e01",getInput(i,"order:addresses:address","line1"));
 commitSegment("N3:N1");
-mapSegment("N4:N1","e01",getInput(i,"addresses:address","city"));
-mapSegment("N4:N1","e02",getInput(i,"addresses:address","state"));
-mapSegment("N4:N1","e03",getInput(i,"addresses:address","zip"));
+mapSegment("N4:N1","e01",getInput(i,"order:addresses:address","city"));
+mapSegment("N4:N1","e02",getInput(i,"order:addresses:address","state"));
+mapSegment("N4:N1","e03",getInput(i,"order:addresses:address","zip"));
 mapSegment("N4:N1","e04","US");
 commitSegment("N4:N1");
 } // if billto
 
-if (getInput(i,"addresses:address","type").equals("shipto")) {
+if (getInput(i,"order:addresses:address","type").equals("shipto")) {
 mapSegment("N1","e01","ST");
-mapSegment("N1","e02",getInput(i,"addresses:address","name"));
+mapSegment("N1","e02",getInput(i,"order:addresses:address","name"));
 mapSegment("N1","e03","92");
-mapSegment("N1","e04",getInput(i,"addresses:address","addrid"));
+mapSegment("N1","e04",getInput(i,"order:addresses:address","addrid"));
 commitSegment("N1");
-mapSegment("N3:N1","e01",getInput(i,"addresses:address","line1"));
+mapSegment("N3:N1","e01",getInput(i,"order:addresses:address","line1"));
 commitSegment("N3:N1");
-mapSegment("N4:N1","e01",getInput(i,"addresses:address","city"));
-mapSegment("N4:N1","e02",getInput(i,"addresses:address","state"));
-mapSegment("N4:N1","e03",getInput(i,"addresses:address","zip"));
+mapSegment("N4:N1","e01",getInput(i,"order:addresses:address","city"));
+mapSegment("N4:N1","e02",getInput(i,"order:addresses:address","state"));
+mapSegment("N4:N1","e03",getInput(i,"order:addresses:address","zip"));
 mapSegment("N4:N1","e04","US");
 commitSegment("N4:N1");
 } // if shipto
 
 }
 
-int itemcount = getGroupCount("items:item");
+int itemcount = getLoopCount("order:items:item",2);
 int total = 0;
 for (int i = 1; i <= itemcount; i++) {
-total += Double.valueOf(getInput(i, "items:item","orderquantity"));
-mapSegment("PO1","e01",getInput(i, "items:item","line"));
-mapSegment("PO1","e02",getInput(i, "items:item","orderquantity"));
-mapSegment("PO1","e03",getInput(i, "items:item","uom"));
-mapSegment("PO1","e04",getInput(i, "items:item","listprice"));
+total += Double.valueOf(getInput(i, "order:items:item","orderquantity"));
+mapSegment("PO1","e01",getInput(i, "order:items:item","line"));
+mapSegment("PO1","e02",getInput(i, "order:items:item","orderquantity"));
+mapSegment("PO1","e03",getInput(i, "order:items:item","uom"));
+mapSegment("PO1","e04",getInput(i, "order:items:item","listprice"));
 mapSegment("PO1","e06","SK");
-mapSegment("PO1","e07",getInput(i, "items:item","skunumber"));
+mapSegment("PO1","e07",getInput(i, "order:items:item","skunumber"));
 mapSegment("PO1","e08","VN");
-mapSegment("PO1","e09",getInput(i, "items:item","itemnumber"));
+mapSegment("PO1","e09",getInput(i, "order:items:item","itemnumber"));
 mapSegment("PO1","e10","UP");
-mapSegment("PO1","e11",getInput(i, "items:item","upcnumber"));
+mapSegment("PO1","e11",getInput(i, "order:items:item","upcnumber"));
 commitSegment("PO1");
 }
 
